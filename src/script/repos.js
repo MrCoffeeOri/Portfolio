e4f31263e1a4afb8d4b60cc32412fee4b90a930c
Renan Penha Marquette <rpenhamarquette@gmail.com>
2021-05-26 09:57:41 -0300

    Updates on API Rest consuming

--- src/script/repos.js
+++ src/script/repos.js
@@ -2,14 +2,19 @@ const Fetch = async (URL) => (await fetch(URL).catch(OnError)).json();
 
 const OnError = (error) => document.getElementById('repositories-box').innerHTML = `<div class="error-box"><h1>An error occurred while loading data !</h1><p>${error}</p></div>`
 
-const CreateRepoBox = (repo) => document.getElementById('repositories-box').innerHTML +=
+const CreateRepoBox = ({name, html_url, description, fork, updated_at, default_branch, watchers, open_issues, license}) => document.getElementById('repositories-box').innerHTML +=
     `<div class="repo-box">
-        <a href="${repo.html_url}"><h1 class="repo-name">${repo.name}</h1></a>
-        <p class="description">${repo.description !== null ? repo.description : ''}</p>
-        <span class="repo-fork">${repo.fork ? 'Forked' : 'Not forked'}</span>
-        <div class="last-updates"><h4>Last update: ${new Date(repo.updated_at)}</h4></div>
+        <a href="${html_url}"><h1 class="repo-name">${name}</h1></a>
+        <p class="description">${description != null ? description : ''}</p>
+        <p class="repo-tag">${license != null ? license.name : 'No license'}</p>
+        <p class="repo-tag">${fork ? 'Forked' : 'Not forked'}</p>
+        ${open_issues > 0 ? `<span style="background-color: rgb(243, 71, 71); color: white">Open issues: ${open_issues}</span>` : `<span>Open issues: ${open_issues}</span>`}
+        <span >Watchers: ${watchers}</span>
+        <span>Default branch: ${default_branch}</span>
+        <div class="last-updates"><h4>Last update: ${new Date(updated_at)}</h4></div>
     </div>`
 
 Fetch('https://api.github.com/users/Coffee-Developer').then(user_data => Fetch(user_data.repos_url).then(user_repos => {
     document.getElementById('title-name').innerText = user_data.login
-    user_repos.map(CreateRepoBox)}))
+    user_repos.map(CreateRepoBox)
+}))
