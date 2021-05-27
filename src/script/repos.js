eb7ac629f081e1672268686ed8c1f7855b88eb0c
Renan Penha Marquette <rpenhamarquette@gmail.com>
2021-05-27 19:06:24 -0300

    New repository box style + more features

--- src/script/repos.js
+++ src/script/repos.js
@@ -5,13 +5,15 @@ const OnError = (error) => document.getElementById('repositories-box').innerHTML
 const CreateRepoBox = ({name, html_url, description, fork, updated_at, default_branch, watchers, open_issues, license}) => document.getElementById('repositories-box').innerHTML +=
     `<div class="repo-box">
         <a href="${html_url}"><h1 class="repo-name">${name}</h1></a>
-        <p class="description">${description != null ? description : ''}</p>
-        <p class="repo-tag">${license != null ? license.name : 'No license'}</p>
-        <p class="repo-tag">${fork ? 'Forked' : 'Not forked'}</p>
-        ${open_issues > 0 ? `<span style="background-color: rgb(243, 71, 71); color: white">Open issues: ${open_issues}</span>` : `<span>Open issues: ${open_issues}</span>`}
-        <span >Watchers: ${watchers}</span>
-        <span>Default branch: ${default_branch}</span>
-        <div class="last-updates"><h4>Last update: ${new Date(updated_at)}</h4></div>
+        <p class="repo-info">${description != null ? description : ''}</p>
+        <div class="tags-box">
+            ${license != null ? `<p style="background-color: #003451;">${license.name}</p>` : '<p style="background-color: #400629;">No license</p>'}
+            ${fork ? '<p style="background-color: #1C6B45;">Forked</p>' : '<p style="background-color: #16553A;">Not forked</p>'}
+            ${open_issues > 0 ? `<p  style="background-color: #721C1C;">Open issues: ${open_issues}</p>` : `<p>Open issues: 0</p>`}
+            ${watchers > 0 ? `<p style="background-color: #1D1D51;">Watchers: ${watchers}</p>` : `<p>Watchers: 0</p>`}
+            ${default_branch == "master" ? `<p style="background-color: #9B6B22;">Default branch: ${default_branch}</p>` : `<p style="background-color: #225864;">Default branch: ${default_branch}</p>`}
+        </div>
+        <p class="repo-info">Last update: ${new Date(updated_at)}</p>
     </div>`
 
 Fetch('https://api.github.com/users/Coffee-Developer').then(user_data => Fetch(user_data.repos_url).then(user_repos => {
